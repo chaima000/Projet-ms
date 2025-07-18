98c314b9fd0635e0dc2e507e2824be024e3e2181
chaima000 <chaimaibrahim622@gmail.com>
2025-07-19 00:43:18 +0100

    33 commit

--- Foyer-ms/src/main/java/tn/esprit/Foyer_ms/FoyerMsApplication.java
+++ Foyer-ms/src/main/java/tn/esprit/Foyer_ms/FoyerMsApplication.java
@@ -2,6 +2,7 @@ package tn.esprit.Foyer_ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -9,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 @SpringBootApplication
-
+@EnableDiscoveryClient
 public class FoyerMsApplication {
 
 	public static void main(String[] args) {
